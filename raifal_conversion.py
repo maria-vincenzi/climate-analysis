4ad9b403b9d961d59b1b57a7fc945f2431c54bd2
Mario Spina <ms915@sussex.ac.uk>
2017-11-09 15:08:41 +0000

    Add rainfall module. per continuare a fare cose random

---
+++ raifal_conversion.py
@@ -0,0 +1,13 @@
+""" a library to perform rainfall unit conversions """
+""" ovvero una libreria per la fuffa"""
+
+
+def inches_to_mm(inches):
+    """ convert inches to mm
+    Argoments:
+    inches --- the rainfall in inches """
+
+    mm = inches * 25.4
+    return mm
+
+
